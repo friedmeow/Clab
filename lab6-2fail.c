e78a9d6ad78b89063710fc51074e66499db5ca76
friedmeow <friedmeow@gmail.com>
2015-10-28 21:34:16 +0800

    Create lab6-2fail.c

---
+++ lab6-2fail.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+int main()
+{
+    char text[100] = {false};
+    int i = 0;
+    scanf("%s", text);
+    for(i = 0; i < 100; i++)
+    {
+        if(i == ',')
+        text[i] = 1;
+        if('A' <= text[i] && text[i] <= 'Z')
+        {
+            ch = ch - 'A';
+        }
+        if('a' <= text[i] && text[i] <= 'z')
+        {
+            ch = ch - 'a';
+        }
+    }
+    
+    for(i = 0; i < 100; i++)
+    {   
+ 
+
+    }
+
+
+}
